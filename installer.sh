740232a711bf2b1de9fdfb80e5d8d12b5de9cecc
bellma101 <bellma@0xfeed.io>
2019-02-28 17:10:02 -0600

    Added notes for installer.sh.

--- installer.sh
+++ installer.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
+# Grep /etc/issue for Ubuntu
+# Check return code of grep
+# If 0, assume Ubuntu
+# Else, repeat with Debian
+# Else, repeat with Kali
+# Else, exit with unknown distro error
 
+# If Ubuntu, run Ubuntu apt-get install list
+sudo apt-get install git wget nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full;
+# If Debian, run Debian apt-get install list
+sudo apt-get install git wget nmap masscan whatweb chromium openssl libnet-ssleay-perl p7zip-full build-essential python-pip python3-pip;
+
+# Run both pip installs
+
+# Create ~/bounty/tools directory
+
+# Clone all repos
+
+# Attempt to install Go
+# Check for Bash shell
+
+# Install go tools
+
+# Install gobuster (wget from github)
+
+# Install aquatone
+
+# Compile massdns
