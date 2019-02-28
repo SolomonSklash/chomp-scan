ed05809adf534ec1f4381a4b1eaa7579d223b054
bellma101 <bellma@0xfeed.io>
2019-02-28 17:25:32 -0600

    Added pip commands to installer.

--- installer.sh
+++ installer.sh
@@ -13,6 +13,8 @@ sudo apt-get install git wget nmap masscan nikto whatweb wafw00f chromium-browse
 sudo apt-get install git wget nmap masscan whatweb chromium openssl libnet-ssleay-perl p7zip-full build-essential python-pip python3-pip;
 
 # Run both pip installs
+sudo pip2 install -r requirements2.txt;
+sudo pip3 install -r requirements3.txt;
 
 # Create ~/bounty/tools directory
 
