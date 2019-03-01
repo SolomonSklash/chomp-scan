4c069660b1d5ee075bcdc1f2ad75976af4f148cb
bellma101 <bellma@0xfeed.io>
2019-02-28 19:18:11 -0600

    All installer functions created.

--- installer.sh
+++ installer.sh
@@ -21,11 +21,30 @@ function install_debian() {
 		echo -e "$GREEN""Installing for Debian.""$NC";
 		# sudo apt-get install git wget nmap masscan whatweb chromium openssl libnet-ssleay-perl p7zip-full build-essential python-pip python3-pip;
 		install_pip;
+		install_dnscan;
+		install_altdns;
+		install_bfac;
+		install_massdns;
+		install_aquatone;
+		install_sublist3r;
+		install_gobuster;
+		install_nikto;
+		install_wafw00f;
+		install_go_tools;
+
 }
 function install_ubuntu() {
 		echo -e "$GREEN""Installing for Ubuntu.""$NC";
 		# sudo apt-get install git wget nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full;
 		install_pip;
+		install_dnscan;
+		install_altdns;
+		install_bfac;
+		install_massdns;
+		install_aquatone;
+		install_sublist3r;
+		install_gobuster;
+		install_go_tools;
 }
 
 function install_pip() {
@@ -55,10 +74,10 @@ function install_massdns() {
 		# git clone https://github.com/blechschmidt/massdns.git "$TOOLS"/massdns;
 		
 		# Compile massdns
-		echo -e "$GREEN""Compiling massdns from source.""$NC";
-		cd "$TOOLS"/massdns;
-		make;
-		cd -;
+		# echo -e "$GREEN""Compiling massdns from source.""$NC";
+		# cd "$TOOLS"/massdns;
+		# make;
+		# cd -;
 }
 
 function install_aquatone() {
@@ -79,13 +98,27 @@ function install_nikto() {
 
 function install_gobuster() {
 		echo -e "$GREEN""Installing gobuster from Github.""$NC";
-		# git clone https://github.com/OJ/gobuster.git "$TOOLS"/gobuster;
-		mkdir -pv "$TOOLS"/gobuster;
-		wget https://github.com/OJ/gobuster/releases/download/v2.0.1/gobuster-linux-amd64.7z -O "$TOOLS"/gobuster/gobuster-linux-amd64.7z;
-		cd "$TOOLS"/gobuster;
-		7z e gobuster-linux-amd64.7z;
-		chmod +x "$TOOLS"/gobuster/gobuster;
-		cd -;
+		# mkdir -pv "$TOOLS"/gobuster;
+		# wget https://github.com/OJ/gobuster/releases/download/v2.0.1/gobuster-linux-amd64.7z -O "$TOOLS"/gobuster/gobuster-linux-amd64.7z;
+		# cd "$TOOLS"/gobuster;
+		# 7z e gobuster-linux-amd64.7z;
+		# chmod +x "$TOOLS"/gobuster/gobuster;
+		# cd -;
+}
+
+function install_wafw00f() {
+		echo -e "$GREEN""Installing wafw00f from Github.""$NC";
+		# git clone https://github.com/EnableSecurity/wafw00f.git "$TOOLS"/wafw00f;
+}
+
+function install_go_tools() {
+		echo -e "$GREEN""Installing Go tools from Github""$NC";
+		echo -e "$GREEN""Installing subfinder from Github""$NC";
+		# go get github.com/subfinder/subfinder;
+		echo -e "$GREEN""Installing subjack from Github""$NC";
+		# go get github.com/haccer/subjack;
+		echo -e "$GREEN""Installing ffuf from Github""$NC";
+		# go get github.com/ffuf/ffuf;
 }
 
 grep 'Ubuntu' /etc/issue 1>/dev/null;
@@ -108,16 +141,9 @@ fi
 # Create ~/bounty/tools directory
 mkdir -pv ~/bounty/tools;
 
-# Clone repos for each distro
-
 # Attempt to install Go
 # Check for Bash shell
 
 # Install go tools
 
-# Install gobuster (wget from github)
-
-# Install aquatone
-
-# Compile massdns
 
