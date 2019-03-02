2223300b49c61945608fbfa2e33ae14e4febea7c
bellma101 <bellma@0xfeed.io>
2019-03-02 00:00:13 -0600

    Removed comments for testing.

--- installer.sh
+++ installer.sh
@@ -26,7 +26,7 @@ function install_kali() {
 }
 function install_debian() {
 		echo -e "$GREEN""Installing for Debian.""$NC";
-		# sudo apt-get install git wget curl nmap masscan whatweb chromium openssl libnet-ssleay-perl p7zip-full build-essential python-pip python3-pip;
+		sudo apt-get install git wget curl nmap masscan whatweb chromium openssl libnet-ssleay-perl p7zip-full build-essential python-pip python3-pip;
 		install_pip;
 		install_dnscan;
 		install_altdns;
@@ -42,7 +42,7 @@ function install_debian() {
 }
 function install_ubuntu() {
 		echo -e "$GREEN""Installing for Ubuntu.""$NC";
-		# sudo apt-get install git wget curl nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full;
+		sudo apt-get install git wget curl nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full;
 		install_pip;
 		install_dnscan;
 		install_altdns;
@@ -58,83 +58,82 @@ function install_ubuntu() {
 function install_pip() {
 		# Run both pip installs
 		 echo -e "$GREEN""Installing requirements for Python 2 and Python 3.""$NC";
-		# sudo pip2 install -r requirements2.txt;
-		# sudo pip3 install -r requirements3.txt;
+		sudo pip2 install -r requirements2.txt;
+		sudo pip3 install -r requirements3.txt;
 }
 
 function install_dnscan() {
 		echo -e "$GREEN""Installing dnscan from Github.""$NC";
-		# git clone https://github.com/rbsec/dnscan.git "$TOOLS"/dnscan;
+		git clone https://github.com/rbsec/dnscan.git "$TOOLS"/dnscan;
 }
 
 function install_altdns() {
 		echo -e "$GREEN""Installing altdns from Github.""$NC";
-		# git clone https://github.com/infosec-au/altdns.git "$TOOLS"/altdns;
+		git clone https://github.com/infosec-au/altdns.git "$TOOLS"/altdns;
 }
 
 function install_bfac() {
 		echo -e "$GREEN""Installing bfac from Github.""$NC";
-		# git clone https://github.com/mazen160/bfac.git "$TOOLS"/bfac;
+		git clone https://github.com/mazen160/bfac.git "$TOOLS"/bfac;
 }
 
 function install_massdns() {
 		echo -e "$GREEN""Installing massdns from Github.""$NC";
-		# git clone https://github.com/blechschmidt/massdns.git "$TOOLS"/massdns;
+		git clone https://github.com/blechschmidt/massdns.git "$TOOLS"/massdns;
 		
 		# Compile massdns
-		# echo -e "$GREEN""Compiling massdns from source.""$NC";
-		# cd "$TOOLS"/massdns;
-		# make;
-		# cd -;
+		echo -e "$GREEN""Compiling massdns from source.""$NC";
+		cd "$TOOLS"/massdns;
+		make;
+		cd -;
 }
 
 function install_aquatone() {
 		echo -e "$GREEN""Installing aquatone from Github.""$NC";
-		# wget https://github.com/michenriksen/aquatone/releases/download/v1.4.3/aquatone_linux_amd64_1.4.3.zip -O "$TOOLS"/aquatone/aquatone.zip;
-		# unzip "$TOOLS"/aquatone/aquatone.zip -d "$TOOLS"/aquatone;
+		wget https://github.com/michenriksen/aquatone/releases/download/v1.4.3/aquatone_linux_amd64_1.4.3.zip -O "$TOOLS"/aquatone/aquatone.zip;
+		unzip "$TOOLS"/aquatone/aquatone.zip -d "$TOOLS"/aquatone;
 }
 
 function install_sublist3r() {
 		echo -e "$GREEN""Installing sublist3r from Github.""$NC";
-		# git clone https://github.com/aboul3la/Sublist3r.git "$TOOLS"/Sublist3r;
+		git clone https://github.com/aboul3la/Sublist3r.git "$TOOLS"/Sublist3r;
 }
 
 function install_nikto() {
 		echo -e "$GREEN""Installing nikto from Github.""$NC";
-		# git clone https://github.com/sullo/nikto.git "$TOOLS"/nikto;
+		git clone https://github.com/sullo/nikto.git "$TOOLS"/nikto;
 }
 
 function install_gobuster() {
 		echo -e "$GREEN""Installing gobuster from Github.""$NC";
-		# mkdir -pv "$TOOLS"/gobuster;
-		# wget https://github.com/OJ/gobuster/releases/download/v2.0.1/gobuster-linux-amd64.7z -O "$TOOLS"/gobuster/gobuster-linux-amd64.7z;
-		# cd "$TOOLS"/gobuster;
-		# 7z e gobuster-linux-amd64.7z;
-		# chmod +x "$TOOLS"/gobuster/gobuster;
-		# cd -;
+		mkdir -pv "$TOOLS"/gobuster;
+		wget https://github.com/OJ/gobuster/releases/download/v2.0.1/gobuster-linux-amd64.7z -O "$TOOLS"/gobuster/gobuster-linux-amd64.7z;
+		cd "$TOOLS"/gobuster;
+		7z e gobuster-linux-amd64.7z;
+		chmod +x "$TOOLS"/gobuster/gobuster;
+		cd -;
 }
 
 function install_wafw00f() {
 		echo -e "$GREEN""Installing wafw00f from Github.""$NC";
-		# git clone https://github.com/EnableSecurity/wafw00f.git "$TOOLS"/wafw00f;
+		git clone https://github.com/EnableSecurity/wafw00f.git "$TOOLS"/wafw00f;
 }
 
 function install_go_tools() {
 		echo -e "$GREEN""Installing Go tools from Github.""$NC";
 		echo -e "$GREEN""Installing subfinder from Github.""$NC";
-		# go get github.com/subfinder/subfinder;
+		go get github.com/subfinder/subfinder;
 		echo -e "$GREEN""Installing subjack from Github.""$NC";
-		# go get github.com/haccer/subjack;
+		go get github.com/haccer/subjack;
 		echo -e "$GREEN""Installing ffuf from Github.""$NC";
-		# go get github.com/ffuf/ffuf;
+		go get github.com/ffuf/ffuf;
 }
 
 function install_go() {
 		echo -e "$GREEN""Installing Go from golang.org.""$NC";
-		# git clone https://github.com/EnableSecurity/wafw00f.git "$TOOLS"/wafw00f;
 		wget https://dl.google.com/go/go1.12.linux-amd64.tar.gz;
 		sudo tar -C /usr/local -xzf go1.12.linux-amd64.tar.gz;
-		echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin:' >> ~/.profile;
+		echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin:" >> ~/.profile;
 		source ~/.profile;
 		rm -rf go1.12.linux-amd64.tar.gz;
 }
@@ -157,4 +156,4 @@ else
 fi
 
 # Create install directory
-# mkdir -pv ~/bounty/tools;
+mkdir -pv ~/bounty/tools;
