72bdbabbd0f9f98048b514b6af16dc874c789ca0
bellma101 <bellma@0xfeed.io>
2019-03-29 08:39:44 -0500

    Added unzip dependency.

--- installer.sh
+++ installer.sh
@@ -14,7 +14,7 @@ TOOLS="$HOME/bounty/tools";
 
 function install_kali() {
 		echo -e "$GREEN""Installing for Kali.""$NC";
-	 	sudo apt-get install git wget curl nmap masscan whatweb sublist3r gobuster nikto wafw00f chromium openssl libnet-ssleay-perl p7zip-full build-essential python-pip python3-pip -y;
+	 	sudo apt-get install git wget curl nmap masscan whatweb sublist3r gobuster nikto wafw00f chromium openssl libnet-ssleay-perl p7zip-full build-essential python-pip python3-pip unzip -y;
 		install_pip;
 		install_dnscan;
 		install_bfac;
@@ -28,7 +28,7 @@ function install_kali() {
 }
 function install_debian() {
 		echo -e "$GREEN""Installing for Debian.""$NC";
-		sudo apt-get install git wget curl nmap masscan whatweb chromium openssl libnet-ssleay-perl p7zip-full build-essential python-pip python3-pip -y;
+		sudo apt-get install git wget curl nmap masscan whatweb chromium openssl libnet-ssleay-perl p7zip-full build-essential python-pip python3-pip unzip -y;
 		install_pip;
 		sudo pip install wafw00f;
 		install_dnscan;
@@ -45,7 +45,7 @@ function install_debian() {
 }
 function install_ubuntu() {
 		echo -e "$GREEN""Installing for Ubuntu.""$NC";
-		sudo apt-get install git wget curl nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full -y;
+		sudo apt-get install git wget curl nmap masscan nikto whatweb wafw00f chromium-browser python-pip python3-pip p7zip-full unzip -y;
 		install_pip;
 		install_dnscan;
 		install_bfac;
