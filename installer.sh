f616e88055c2cafdb4ea0911e66a9a168fa6a0ba
bellma101 <bellma@0xfeed.io>
2019-03-30 18:55:13 -0500

    Added -u to go get to update tools if installer.sh is run more than once.

--- installer.sh
+++ installer.sh
@@ -131,21 +131,21 @@ function install_go_tools() {
 		echo -e "$GREEN""Installing Go tools from Github.""$NC";
 		sleep 5;
 		echo -e "$GREEN""Installing subfinder from Github.""$NC";
-		go get github.com/subfinder/subfinder;
+		go get -u github.com/subfinder/subfinder;
 		echo -e "$GREEN""Installing subjack from Github.""$NC";
-		go get github.com/haccer/subjack;
+		go get -u github.com/haccer/subjack;
 		echo -e "$GREEN""Installing ffuf from Github.""$NC";
-		go get github.com/ffuf/ffuf;
+		go get -u github.com/ffuf/ffuf;
 		echo -e "$GREEN""Installing gobuster from Github.""$NC";
-		go get github.com/OJ/gobuster;
+		go get -u github.com/OJ/gobuster;
 		echo -e "$GREEN""Installing inception from Github.""$NC";
-		go get github.com/proabiral/inception;
+		go get -u github.com/proabiral/inception;
 		echo -e "$GREEN""Installing waybackurls from Github.""$NC";
-		go get github.com/tomnomnom/waybackurls;
+		go get -u github.com/tomnomnom/waybackurls;
 		echo -e "$GREEN""Installing goaltdns from Github.""$NC";
-		go get github.com/subfinder/goaltdns;
+		go get -u github.com/subfinder/goaltdns;
 		echo -e "$GREEN""Installing rescope from Github.""$NC";
-		go get github.com/root4loot/rescope;
+		go get -u github.com/root4loot/rescope;
 }
 
 function install_go() {
