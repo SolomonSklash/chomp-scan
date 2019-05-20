a94090b1fb98e4e40f03013ab4e095e05b2d05c3
bellma101 <bellma@0xfeed.io>
2019-05-20 12:55:39 -0600

    Bumped amass installer note to 2.9.12.

--- installer.sh
+++ installer.sh
@@ -254,10 +254,10 @@ function install_amass() {
 		if [[ -d "$TOOLS"/amass ]]; then
 				rm -rf "$TOOLS"/amass;
 		fi
-		echo -e "$GREEN""[+] Installing amass 2.9.11 from Github.""$NC";
-		wget https://github.com/OWASP/Amass/releases/download/2.9.11/amass_2.9.11_linux_amd64.zip -O "$TOOLS"/amass.zip;
+		echo -e "$GREEN""[+] Installing amass 2.9.12 from Github.""$NC";
+		wget https://github.com/OWASP/Amass/releases/download/2.9.12/amass_2.9.12_linux_amd64.zip -O "$TOOLS"/amass.zip;
 		unzip -o "$TOOLS"/amass.zip -d "$TOOLS";
-		mv "$TOOLS"/amass_2.9.11_linux_amd64 "$TOOLS"/amass;
+		mv "$TOOLS"/amass_2.9.12_linux_amd64 "$TOOLS"/amass;
 		rm "$TOOLS"/amass.zip;
 }
 
