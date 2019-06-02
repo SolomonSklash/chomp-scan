eaed7cd90779bc989f3f6663aaea5d00aead9a8d
Emad Youssef <48482029+Sy3Omda@users.noreply.github.com>
2019-06-02 13:21:50 +0200

    update amass

--- installer.sh
+++ installer.sh
@@ -255,7 +255,7 @@ function install_amass() {
 				rm -rf "$TOOLS"/amass;
 		fi
 		echo -e "$GREEN""[+] Installing amass 2.9.12 from Github.""$NC";
-		wget https://github.com/OWASP/Amass/releases/download/2.9.12/amass_2.9.12_linux_amd64.zip -O "$TOOLS"/amass.zip;
+		wget https://github.com/OWASP/Amass/releases/download/3.0.3/amass_3.0.3_linux_amd64.zip -O "$TOOLS"/amass.zip;
 		unzip -o "$TOOLS"/amass.zip -d "$TOOLS";
 		mv "$TOOLS"/amass_2.9.12_linux_amd64 "$TOOLS"/amass;
 		rm "$TOOLS"/amass.zip;
