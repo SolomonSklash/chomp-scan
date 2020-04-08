a1fe9f20c2586bdf4ac154a6e9b621677556b513
Solomon Sklash <solomonsklash@0xfeed.io>
2020-04-08 10:38:59 -0500

    Bumped amass version to 3.5.4.

--- install.sh
+++ install.sh
@@ -261,8 +261,8 @@ function install_amass() {
 		if [[ -d "$TOOLS"/amass ]]; then
 				rm -rf "$TOOLS"/amass;
 		fi
-		echo -e "$GREEN""[+] Installing amass 3.1.9 from Github.""$NC";
-		wget -nv https://github.com/OWASP/Amass/releases/download/v3.1.9/amass_v3.1.9_linux_amd64.zip -O "$TOOLS"/amass.zip;
+		echo -e "$GREEN""[+] Installing amass 3.5.4 from Github.""$NC";
+		wget -nv https://github.com/OWASP/Amass/releases/download/v3.5.4/amass_v3.5.4_linux_amd64.zip -O "$TOOLS"/amass.zip;
 		unzip -j "$TOOLS"/amass.zip -d "$TOOLS"/amass;
 		rm "$TOOLS"/amass.zip;
 }
